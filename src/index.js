4d332c461cef6d48135bf65511265edd11d6db2b
yuliyabondar <yuliabondar98@gmail.com>
2021-08-16 16:07:30 +0300

    docs: update indexJs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
-}
+    if (number < 0 || number > 999) {
+        return false;
+    } 
+    var units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const convertTens = (number) => {
+        if (number < 10) {
+            return units[number];
+        } else if (number >= 10 && number < 20) {
+            return teens[number - 10];
+        } else {
+            result1 = `${tens[Math.floor(number / 10)]} ${units[number % 10]}`;
+            return result1.trim();
+        }
+    };
+    if (number > 99) {       
+        result2 = `${units[Math.floor(number / 100)]} hundred ${convertTens(number % 100)}`;
+        return result2.trim();
+    } else if (number === 0) {
+        return 'zero';
+    } else {
+        return convertTens(number);
+    }
+    }
